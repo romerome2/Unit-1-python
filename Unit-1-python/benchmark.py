f15a8e8c797960c280ccd6df8ed65b8be6561485
romerome2 <jspooner@brookyllnsteamcenter.org>
2023-09-19 09:52:17 -0400

    excersie test

---
+++ Unit-1-python/benchmark.py
@@ -0,0 +1,32 @@
+# Excersise 1:Display your name in the console
+
+print("Jerome")
+
+# Excersise 2: 
+favoriteShow = "Regular Show"
+
+# Excersie 3:
+height = "6'0"
+age = "17"
+
+# Excercise 4:
+print("im going to break")
+
+# Excersise 5:
+fav_food =("cheeseburgers,sushi,watermelmon")
+
+# Excersise 6:
+if age >= 21:
+    print("you can rent a car")
+else:
+  print("your beat")
+    
+   # Excercise 7:
+input("please enter your name")
+
+
+
+ # Excercise 8:
+
+ # Excersise 9:
+
