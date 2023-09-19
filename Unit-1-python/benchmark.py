09d262678c303706887639f9dc396e022ba345cf
romerome2 <jspooner@brookyllnsteamcenter.org>
2023-09-19 10:51:23 -0400

    fxied my excersises errors

--- Unit-1-python/benchmark.py
+++ Unit-1-python/benchmark.py
@@ -6,27 +6,29 @@ print("Jerome")
 favoriteShow = "Regular Show"
 
 # Excersie 3:
-height = "6'0"
-age = "17"
+height = 6.0
+age = 17
 
 # Excercise 4:
-print("im going to break")
+print(im going to break)
 
 # Excersise 5:
-fav_food =("cheeseburgers,sushi,watermelmon")
+fav_food =['cheeseburgers','sushi','watermelmon']
 
 # Excersise 6:
 if age >= 21:
-    print("you can rent a car")
+    print('you can rent a car')
 else:
   print("your beat")
     
    # Excercise 7:
-input("please enter your name")
+Their_Name = input("Ehat is your name? ")
+print ("Hello," + Their_Name)
 
-
-
- # Excercise 8:
-
+# Excercise 8:
+for i in fav_food :
+   print(i)
  # Excersise 9:
-
+x = 1 
+while  x == 2 :
+   print("equal >:)")
