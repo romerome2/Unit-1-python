deb4083768a5c80211780884e16c2c823bfba3a1
romerome2 <jspooner@brookyllnsteamcenter.org>
2023-09-20 10:53:06 -0400

    more excersises

---
+++ Unit-1-python/data_types.py
@@ -0,0 +1,43 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+x=14.0
+print(x)
+print(int(x))
+# For this task i made a varibale then i just turned it into a integer by adding int.
+""" 
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+c=input("what is the number")
+if c > 0:
+    print("postive")
+elif c < 0:
+    print("negative")# I made a variable which was c 
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
