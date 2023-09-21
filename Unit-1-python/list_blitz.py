375d748c447bf542bdd06b401375014127a63aa4
romerome2 <jspooner@brookyllnsteamcenter.org>
2023-09-21 10:53:13 -0400

    added

---
+++ Unit-1-python/list_blitz.py
@@ -0,0 +1,61 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+
+
+fruits=["orange","apple","chicken","durian"]
+print(fruits)
+#I made a list called fruit then i added 4 elements then i printed it.
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+fruits.append("orange")
+print(fruits)
+# I added the word append which added a element to the end of a list.
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+fruits.remove("chicken")
+print(fruits)
+# I removed my item from a list by doing fruits.remove which would remove an item.
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+fruits[2] = "whatever"
+print(fruits)
+# I made an element then i made it modify the list
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+fruits.append("dragonfruit")
+fruits.append("grapes")
+fruits.append("cherry")
+print(fruits)
+# I added 3 more fruits to my list using append.
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+del fruits[-1]#deleltes the last item on the list i assume.
+print(fruits)# this prints what i have in my list.
+
+"""
+Task 7: Create a List with a Range of Numbers
+Create a list containing numbers within a specified range 
+(inclusive). Print the created list.
+"""
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
