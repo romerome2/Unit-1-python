ba4f5a60b2d85fb506eb5bc7549bb4f9749127cd
romerome2 <jspooner@brookyllnsteamcenter.org>
2023-10-13 10:53:38 -0400

    return of the function work

---
+++ Unit-1-python/return_of_the_function.py
@@ -0,0 +1,36 @@
+"""
+Task 1: Calculate the Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+def add_num(a):
+    return a * a
+x = add_num(10)
+print(x)
+
+# I made a program that times the variable by it self to bet its answer
+
+"""
+Task 2: Calculate the Area of a Rectangle:
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+def add_area(a,b):
+    return a * b
+x = add_area(12,4)
+print(x)
+# Basically in this function we just multiplied the variable value and got an answer.
+
+"""
+Task 3: Convert Temperature from Celsius to Fahrenheit:
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+def con_cf(b):
+    return b * 9 / 5 + 32
+x = con_cf(95)
+print(x)
+ # I made a program tha calculates the fahrenheit.
+"""
+Task 4: Calculate the Average of Numbers:
+Write a function that takes a list of numbers 
+and returns the average (mean) of those numbers.
+"""
